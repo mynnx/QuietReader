c76e1cef19c24d35911cc93c340e743282c72f20
Mark Everett <themynnx@gmail.com>
2010-11-06 21:49:18 +0000

    Implemented session authentication as recommended in the Dancer Cookbook [1].  Now /feeds and / require authentication, and are redirected to /login if the session isn't set up.

--- lib/QuietReader.pm
+++ lib/QuietReader.pm
@@ -14,24 +14,16 @@ sub make_reader {
 	return $reader;
 }
 
-get '/' => sub { qq!
-	<form action="/tags" method="POST">
-		<input type="text" name="username" /><br>
-		<input type="password" name="password" /><br>
-		<input type="submit" value="Log in" />
-	</form>!;
+before sub {
+	if (! session('user') && request->path_info !~ m{^/login}) {
+		var requested_path => request->path_info;
+		request->path_info('/login');
+	}
 };
 
-post '/tags' => sub {
-	session user => params->{username};
-	session pass => params->{password};
-	my $user = session->{user};
-	my $pass = session->{pass};
+sub render_feeds {
+	my $reader = make_reader(session('user'), session('pass'));
 	my $rv = "";
-	my $reader = make_reader($user, $pass);
-	debug "About to log in as $user with password $pass";
-	debug "error: ${reader->error}" if $reader->error;
-
 	my @tags = $reader->tags;
 	foreach my $tag (@tags) {
 		my $id = $tag->id;
@@ -42,5 +34,45 @@ post '/tags' => sub {
 	}
 	return $rv;
 };
+
+get '/' => \&render_feeds;
+get '/feeds' => \&render_feeds;
+
+get '/login' => sub { 
+	my $path_requested = vars->{requested_path} || '/';
+	my $rv = qq!
+	<form action="/login" method="POST">
+		<input type="text" name="username" /><br>
+		<input type="password" name="password" /><br>
+		<input type="hidden" name="path" value="$path_requested" />
+		<input type="submit" value="Log in" />
+	</form>!;
+	debug "LOGIN";
+	$rv .= "Error authenticating!" if params->{failed};
+	return $rv;
+};
+
+post '/login' => sub {
+	my $user = params->{username};
+	my $pass = params->{password};
+	my $rv = "";
+	my $reader = make_reader($user, $pass);
+
+	# Call ->tags, which may actually cause an ->error; creating a new 
+	# 	::Reader doesn't actually hit the API, I don't think.
+	my @tags = $reader->tags;
+	if ($reader->error) {
+		redirect '/login?failed=1';
+	} else {
+		session user => $user,
+		session pass => $pass,
+		# TODO generate/store OTP, encrypt password with it and put in cookie
+		# - clear your session persistent store; passwords are plaintext for now!
+		redirect params->{path} || '/';
+	}
+	return $rv;
+};
+
+
 
 true;
