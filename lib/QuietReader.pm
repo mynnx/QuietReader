b93e310c47f5ed7d33060796357038c6bacfca1d
Naveed Massjouni <naveedm9@gmail.com>
2010-11-06 05:17:50 -0400

    Adding templates.

--- lib/QuietReader.pm
+++ lib/QuietReader.pm
@@ -14,33 +14,21 @@ sub make_reader {
 	return $reader;
 }
 
-get '/' => sub { qq!
-	<form action="/tags" method="POST">
-		<input type="text" name="username" /><br>
-		<input type="password" name="password" /><br>
-		<input type="submit" value="Log in" />
-	</form>!;
-};
+get '/' => sub { template 'login' };
 
 post '/tags' => sub {
 	session user => params->{username};
 	session pass => params->{password};
 	my $user = session->{user};
 	my $pass = session->{pass};
-	my $rv = "";
-	my $reader = make_reader($user, $pass);
-	debug "About to log in as $user with password $pass";
-	debug "error: ${reader->error}" if $reader->error;
 
-	my @tags = $reader->tags;
-	foreach my $tag (@tags) {
-		my $id = $tag->id;
-		my $name = $id;
-		$name =~ s|.+/.+/label/(.+)|$1|;
-		$name =~ s|.+/.+/state/.+/(.+)|$1|;
-		$rv .= $name . '<br>';
-	}
-	return $rv;
+	debug "About to log in as $user with password $pass";
+	my $reader = make_reader($user, $pass);
+	debug "error: " . $reader->error if $reader->error;
+
+    template tags => {
+        names => [ map { (split '/', $_->id)[-1] } $reader->tags ]
+    };
 };
 
 true;
